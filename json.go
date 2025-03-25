921a7cfdd1783b10081043837f7d30a4f6d9f211
Giovanni Usai <g.usai@balanceconsulting.it>
2025-03-25 17:44:53 +0100

    exercise

--- json.go
+++ json.go
@@ -30,5 +30,9 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-	w.Write(dat)
+	n, err := w.Write(dat)
+	if err != nil {
+		log.Printf("Error writing %v || Bytes written %d)", err, n)
+	}
+
 }
